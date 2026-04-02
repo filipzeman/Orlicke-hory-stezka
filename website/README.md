60dfc05911c8f7eab0e29b9e366c323d1fff16bf
Filip Zeman <zeman.filip@gmail.com>
2026-04-02 06:54:20 +0200

    Add image upload to supabase, login, admin, fetching images for detai page

--- website/README.md
+++ website/README.md
@@ -1,11 +1,10 @@
 # Digitání průvodce etapou Stezky Českem
 
-### Version: 0.2
+### Version: 0.5
 
 ### Author: Filip Zeman
 
 ## TODO:
 
--- when navigation category active, display lines between points with color based on the route (red, blue, green, yellow, default)
 -- PWA (offline data)
 -- option to download filtered itinerary
---
+++ website/src/lib/supabaseServer.ts
@@ -0,0 +1,18 @@
+import { createServerClient } from "@supabase/ssr";
+
+export function createSupabaseServerClient(Astro: any) {
+  const url = import.meta.env.SUPABASE_URL;
+  const key = import.meta.env.SUPABASE_ANON_KEY;
+
+  if (!url || !key) {
+    throw new Error("Missing Supabase env variables");
+  }
+
+  return createServerClient(url, key, {
+    cookies: {
+      get: (name) => Astro.cookies.get(name)?.value,
+      set: (name, value, options) => Astro.cookies.set(name, value, options),
+      remove: (name, options) => Astro.cookies.delete(name, options),
+    },
+  });
+}
