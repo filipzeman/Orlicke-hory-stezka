72193d4128f49c16349e9f4b8bbe68d04d11edf8
Filip Zeman <zeman.filip@gmail.com>
2026-04-08 11:32:32 +0200

    header and footer update, admin, etc.

--- website/src/constants/constants.ts
+++ website/src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const ETAPE_LENGTH: number = 93.1;
 
-export const FIRST_LEVEL_NAVIGATION: string[] = ["Itinerář", "Mapa", "Blog", "O projektu"];
+export const FIRST_LEVEL_NAVIGATION: string[] = ["Itinerář", "Mapa", "Blog", "Časté dotazy"];
